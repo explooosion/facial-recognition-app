abf9c0fcf4454c73216ee375871d292e00fcb67b
Robby <ta7382@gmail.com>
2019-08-22 01:57:21 +0800

    Add demo web

---
+++ docs/precache-manifest.25fc362e2aed9130c27a8a0469cced57.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0b9ca88ecf088e2e571c0d91b7837fe",
+    "url": "/facial-recognition-app/index.html"
+  },
+  {
+    "revision": "1e6f546de558819f429c",
+    "url": "/facial-recognition-app/static/css/main.848e611f.chunk.css"
+  },
+  {
+    "revision": "eb227acdc765c9023817",
+    "url": "/facial-recognition-app/static/js/2.e733767b.chunk.js"
+  },
+  {
+    "revision": "1e6f546de558819f429c",
+    "url": "/facial-recognition-app/static/js/main.0ded8efd.chunk.js"
+  },
+  {
+    "revision": "9548c6c4f99b2bb96115",
+    "url": "/facial-recognition-app/static/js/runtime~main.3b6f9df2.js"
+  },
+  {
+    "revision": "34e86f06102f347f0475d68f8673318b",
+    "url": "/facial-recognition-app/static/media/test.34e86f06.jpg"
+  }
+]);
